84c9a54987f2b5e1f57642234d69a151e3d7ed69
ajmanipriyancy <ajmanipriyancy@gmail.com>
2025-11-29 00:15:57 +0530

    Implement two-sum problem solution in add2.cpp

---
+++ add2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, target;
+    cin >> n >> target;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++) cin >> arr[i];
+    unordered_set<int> s;
+    for(int x : arr) {
+        if(s.count(target - x)) {
+            cout << "true";
+            return 0;
+        }
+        s.insert(x);
+    }
+    cout << "false";
+}
